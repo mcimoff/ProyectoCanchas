32105f47c36c2dcc41a5c2980cf619c1302cfcf1
Matias Imoff <matias.c.imoff@gmail.com>
2022-06-26 19:19:18 -0300

    Modificaciones en Reserva Santiago y Cancha Matias

--- routes/canchas.js
+++ routes/canchas.js
@@ -7,9 +7,40 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async(req,res) =>{
-    const nuevoTurno = req.body;
-    const resultado = await controller.agregarTurno(nuevoTurno);
+    const nuevoCancha = req.body;
+    const resultado = await controller.agregarCancha(nuevoCancha);
     res.json(resultado);
+})
+
+router.delete('/borrarCancha/:id', async(req,res) =>{
+    const cancha = await controller.borrarCancha(req.params.id);
+    res.json(cancha);
+})
+
+router.get('/size/:size', async(req,res) => {
+    const size = await controller.sizeCancha(req.params.size);
+    res.json(size);
+})
+
+router.put('/modificarNombre/:id', async(req,res) =>{
+    const modificarNombre = await controller.modificarNombreCancha(req.params.id);
+    res.json(modificarNombre);
+})
+
+router.put('/modificarSize/:id', async(req,res) =>{
+    const modificarSize = await controller.modificarSize(req.params.id);
+    res.json(modificarSize);
+})
+
+router.put('/modificarPrecio/:id', async(req,res) =>{
+    const modificarPrecio = await controller.modificarPrecio(req.params.id);
+    res.json(modificarPrecio);
+})
+
+
+router.get('/:id', async(req,res) =>{
+    const cancha = await controller.getCancha(req.params.id);
+    res.json(cancha);
 })
 
 module.exports = router;
